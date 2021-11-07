616b8ce2c0eee6b1f0ad537b85bee4c5aa6bc59c
Bornyto Hamonangan <70834922+hamonangann@users.noreply.github.com>
2021-11-07 22:40:03 +0700

    Update perkalian.py
    Update deskripsi supaya lebih menarik

--- perkalian.py
+++ perkalian.py
@@ -4,8 +4,8 @@ print("Selamat datang di perkalian.py")
 
 play = True
 while (play):
-    print("1. Main")
-    print("2. Petunjuk")
+    print("1. Main!")
+    print("2. Trik rahasia!")
     print("3. Exit")
     mode = input("Pilih (1-3): ")
 
@@ -26,10 +26,10 @@ while (play):
 
                 if (expected == answer):
                     print("Hore benar!")
-                    print("Karena", left, "x", right, "=", ((left-1) * right), "+", right, "=", (left * right), "ya!")
+                    # print("Karena", left, "x", right, "=", ((left-1) * right), "+", right, "=", (left * right), "ya!")
                 else:
-                    print("Yah, salah... Karena", left-1, "x", right, "=", ((left-1) * right))
-                    print("Maka", left, "x", right, "=", ((left-1) * right), "+", right, "=", (left * right), "ya!")
+                    print("Yah, salah... yang benar adalah", expected, "ya!") #Karena", left-1, "x", right, "=", ((left-1) * right))
+                    # print("Maka", left, "x", right, "=", ((left-1) * right), "+", right, "=", (left * right), "ya!")
 
                     lives -= 1
                     print("Sisa nyawa:", lives)
@@ -43,10 +43,10 @@ while (play):
 
                 if (expected == answer):
                     print("Hore benar!")
-                    print("Karena", left, "x", right, "=", (left * (right-1)), "+", left, "=", (left * right), "ya!")
+                    # print("Karena", left, "x", right, "=", (left * (right-1)), "+", left, "=", (left * right), "ya!")
                 else:
-                    print("Yah, salah... Karena", left, "x", right-1, "=", (left * (right-1)))
-                    print("Maka", left, "x", right, "=", (left * (right-1)), "+", left, "=", (left * right), "ya!")
+                    print("Yah, salah... yang benar adalah", expected, "ya!") # Karena", left, "x", right-1, "=", (left * (right-1)))
+                    # print("Maka", left, "x", right, "=", (left * (right-1)), "+", left, "=", (left * right), "ya!")
 
                     lives -= 1
                     print("Sisa nyawa:", lives)
@@ -54,21 +54,29 @@ while (play):
             level += 1
             print()
 
-        print("Game selesai. Selamat, kamu sampai level", level)
+        print("Game selesai. Selamat, kamu sampai level", level, "!")
 
     elif (mode == "2"):
-        print("Ketimbang menghafal dengan tabel, belajar perkalian lebih menyenangkan dengan latihan penjumlahan\n")
+        print(20*('='), "TRIK RAHASIA", 20*('='))
+        print("Rahasia game ini (dan juga perkalian pada dunia nyata): perkalian adalah penjumlahan berulang!")
+        print("Sebagai contoh, 3 x 4 = 4 + 4 + 4 (4-nya ada 3) = 12. Bisa juga 3 x 4 = 3 + 3 + 3 + 3 (3-nya ada 4) = 12")
+        print()
 
-        print("Misalnya: diketahui 4 x 3 = 12, berapa hasil 5 x 3?\n")
+        print("Nah, dengan memerhatikan jawaban soal sebelumnya, kamu bisa menjawab pertanyaan lebih cepat!")
+        print("Misalnya, sebelumnya ditanyakan 2 x 5 = 10")
+        print()
 
-        print("Jawab: Karena yang bertambah adalah bilangan di sebelah kiri, maka kita tambahkan hasil sebelumnya dengan bilangan di sebelah kanan")
-        print("Jawabannya adalah 5 x 3 = (4 x 3) + 3 = 12 + 3 = 15\n")
-        
-        print("Misalnya: diketahui 5 x 2 = 10, berapa hasil 5 x 3?\n")
+        print("Kalau kamu ditanya berapa 3 x 5, maka ingat bahwa 2 x 5 itu 5-nya 2 kali, sedangkan 3 x 5 itu 5-nya 3 kali")
+        print("Artinya, karena 2 x 5 = 5 + 5, maka 3 x 5 = 5 + 5 + 5 = (2 x 5) + 5 = 10 + 5")
+        print("Jadi, kamu hanya perlu menambahkan 5 dari jawaban sebelumnya, sehingga didapatlah hasil 3 x 5 = 15")
+        print()
 
-        print("Jawab: Nah, sekarang yang bertambah itu bilangan sebelah kanan, maka kita tambahkan hasil sebelumnya dengan bilangan sebelah kiri")
-        print("Jangan sampai terbalik ya!")
-        print("Jadi jawabannya adalah 5 x 3 = (5 x 2) + 5 = 10 + 5 =15\n")
+        print("Nah, kalau kamu ditanya berapa 2 x 6, maka ingat 2 x 5 itu 2-nya 5 kali, sedangkan 2 x 6 itu 2-nya 6 kali")
+        print("Mirip tadi, karena 2 x 5 = 2 + 2 + 2 + 2 + 2, maka 2 x 6 = 2 + 2 + 2 + 2 + 2 + 2 = (2 x 5) + 2 = 10 + 2")
+        print("Jadi, kamu hanya perlu menambahkan 2 dari jawaban sebelumnya, sehingga hasilnya adalah 2 x 6 = 12")
+        print()
+
+        print(20*('='), "Good luck!", 20*('='))
 
     elif (mode == "3"):
         play = False
